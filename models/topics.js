c494e0fcbb05c9dcfcf02295505d529676377ba7
Evan McCoy <evan.mccoy@gmail.com>
2019-06-03 14:18:49 -0400

    placed inputs into table

--- models/topics.js
+++ models/topics.js
@@ -22,6 +22,36 @@ class topics {
             return err
         }
     }
+
+    static async updateAllRanks(topicArray, newRankArray) {
+        let query = `update topics as t set
+                        number = c.number
+                        from (values
+                            ('${topicArray[0]}', ${newRankArray[0]}),
+                            ('${topicArray[1]}', ${newRankArray[1]}),
+                            ('${topicArray[2]}', ${newRankArray[2]}),
+                            ('${topicArray[3]}', ${newRankArray[3]}),
+                            ('${topicArray[4]}', ${newRankArray[4]}),
+                            ('${topicArray[5]}', ${newRankArray[5]})
+                        ) as c(topic, number)
+                        where t.topic = c.topic`;
+        // console.log("Update Query", query);
+        try {
+            let response = await db.result(query);
+            return response;
+        } catch(err) {
+            console.log("ERROR", err.message);
+            return err
+        }
+    }
 }
 
 module.exports = topics;
+
+// update topics as t set
+//     number = c.number
+// from (values
+//     ('Node', 5),
+//     ('Express', 0)
+// ) as c(topic, number)
+// where t.topic = c.topic;
